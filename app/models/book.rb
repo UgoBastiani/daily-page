75f7eb934e138b959aa15a2b2ce071f729859d9f
Thierry Luu <thierry.luu@gmail.com>
2024-08-19 17:48:54 +0200

    model associations et validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,9 @@ class Book < ApplicationRecord
   belongs_to :genre
   has_many :suggestions
   has_many :favorites
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :number_of_pages, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :publishing_date, presence: true
 end
