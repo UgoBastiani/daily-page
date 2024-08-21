8f99ab76bace1a07aa4b6426278f4474138f3e4b
Ugo Bastiani <ugo.bastiani@hotmail.com>
2024-08-21 17:18:39 +0200

    3 questions added

---
+++ app/controllers/suggestions_controller.rb
@@ -0,0 +1,31 @@
+class SuggestionsController < ApplicationController
+
+  def index
+    @books = Book.all
+
+    if params[:mood].present?
+      @books = @books.joins(:category).where("categories.mood ILIKE ?", params[:mood])
+    end
+
+    if params[:pages].present?
+      case params[:pages].downcase
+      when 'short'
+        @books = @books.where('number_of_pages < ?', 250)
+      when 'medium'
+        @books = @books.where('number_of_pages >= ? AND number_of_pages <= ?', 250, 400)
+      when 'big'
+        @books = @books.where('number_of_pages > ?', 400)
+      when 'anyway'
+      end
+    end
+
+    if params[:age].present?
+      case params[:age].downcase
+      when 'old'
+        @books = @books.where('publishing_date < ?', Date.new(2010))
+      when 'new'
+        @books = @books.where('publishing_date >= ?', Date.new(2010))
+      end
+    end
+  end
+end
