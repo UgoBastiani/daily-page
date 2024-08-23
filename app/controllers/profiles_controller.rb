52705bc81e19fde48395acdb7908cdb8e70f9b71
Gaelle Lafargue <gaellelfrgue@gmail.com>
2024-08-23 12:08:49 +0200

    profiles controller changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,5 @@ class ProfilesController < ApplicationController
     @suggestions = current_user.suggestions
     @books_read = current_user.favorites.where(read: true)
     @pages_read = current_user.favorites.where(read: true).includes(:book).sum { |favorite| favorite.book.number_of_pages }
-
   end
 end
