fe51a9885f478bc94996fafa21b1995cefb10af6
Ugo Bastiani <ugo.bastiani@hotmail.com>
2024-08-23 11:04:51 +0200

    read and unread added

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,6 @@
 class FavoritesController < ApplicationController
+  before_action :set_favorite, only: [:mark_as_read, :mark_as_unread]
+
   def create
     @favorite = Favorite.new
     @favorite.user = current_user
@@ -10,7 +12,28 @@ class FavoritesController < ApplicationController
     @favorites = Favorite.where(user: current_user)
   end
 
+
+  def mark_as_read
+    if @favorite.update(read: true)
+      redirect_to favorites_path, notice: 'The book has been marked as read.'
+    else
+      redirect_to favorites_path, alert: 'An error has occurred, please try again.'
+    end
+  end
+
+  def mark_as_unread
+    if @favorite.update(read: false)
+      redirect_to favorites_path, notice: 'The book has been marked as unread.'
+    else
+      redirect_to favorites_path, alert: 'An error has occurred, please try again.'
+    end
+  end
+
   private
+
+  def set_favorite
+    @favorite = Favorite.find(params[:id])
+  end
 
   def favorite_params
     params.require(:favorite).permit(:read)
